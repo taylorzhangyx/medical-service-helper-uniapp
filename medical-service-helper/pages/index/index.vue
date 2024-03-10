cbf075de56832308c69554ba5b5025b232d10698
tayzhang <tayzhang@tencent.com>
2024-03-10 18:16:01 +0800

    reactive and conputed value code to refresh view auto
    Signed-off-by: tayzhang <tayzhang@tencent.com>

--- medical-service-helper/pages/index/index.vue
+++ medical-service-helper/pages/index/index.vue
@@ -4,23 +4,61 @@
 		<view class="text-area">
 			<text class="title">{{title}}</text>
 		</view>
+		<text>total count: {{totalCount}}</text>
+		<button type="default" @click="handleClick">click me!</button>
+		<view v-for="item in list" :key="item.name">
+			<text>name is: {{item.name}}\n</text>
+			<text>number is: {{item.num}}</text>
+		</view>
 	</view>
 </template>
 
-<script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
+<script setup>
+	// export default {
+	// 	data() {
+	// 		return {
+	// 			title: 'Hello'
+	// 		}
+	// 	},
+	// 	onLoad() {
 
-		},
-		methods: {
+	// 	},
+	// 	methods: {
 
-		}
+	// 	}
+	// }
+	import { ref, reactive, computed} from "vue"
+	import { onLoad } from "@dcloudio/uni-app"
+	const title = ref("Hello")
+	const list = reactive([
+		{name: 'apple', num: 2},
+		{name: 'banana', num: 4},
+		{name: 'orange', num: 6}
+	])
+	var totalCount = computed(()=>{
+			return list.reduce((total, cur) => {
+				console.log('total: ', total);
+				console.log('cur.num: ', cur.num);
+				return total + cur.num;
+			}, 0)
+	})
+	const handleClick = ()=>{
+		// let n = 0
+		list.forEach(item =>{
+			item.num++
+		})
+		// totalCount = n
 	}
+	
+	onLoad(()=>{
+		console.log("onLoad life cycle123")
+		// var n = 0
+		// list.forEach(item =>{
+		// 	n = n+item.num
+		// })
+		// totalCount = n
+	})
+	
 </script>
 
 <style>
