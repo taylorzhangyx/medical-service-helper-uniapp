10665b8b2833fd203e5848358e887feb8d935efa
tayzhang <tayzhang@tencent.com>
2024-03-18 22:59:03 +0800

    make navbar custom
    Signed-off-by: tayzhang <tayzhang@tencent.com>

--- medical-service-helper/components/navbar/navbar.vue
+++ medical-service-helper/components/navbar/navbar.vue
@@ -1,83 +1,86 @@
 <template>
     <view class="nav">
-        <view :style="'height'+statusHeight+'rpx;' + containerStyle"></view>
-        <view class="navbar" :style="'height:'+navHeight + 'rpx;' + containerStyle"></view>
+        <view class="statusbar"
+              :style="'height:' + statusHeight + 'rpx;' + containerStyle">statusbar</view>
+        <view class="navbar"
+              :style="'height:' + navHeight + 'rpx;' + containerStyle">navbar</view>
         <view>this is taylor's navbar</view>
     </view>
 </template>
 
 <script setup>
-    import {
-        onBeforeMount,
-        ref
-    } from 'vue'
-    const props = defineProps({
-        background: {
-            type: String,
-            default: 'rgba(255, 255, 255, 1)'
-        },
-        color: {
-            type: String,
-            default: 'rgba(0,0,0,1)'
-        },
-        fontSize: {
-            type: String,
-            default: '32'
-        },
-        iconWidth: {
-            type: String,
-            default: '116'
-        },
-        iconHeight: {
-            type: String,
-            default: '38'
-        }
-    })
-    onBeforeMount(() => {
-        console.log('onBeforeMount')
-        setNavSize()
-        setStyle()
-    })
-    // 状态栏高度
-    const statusHeight = ref(0)
-    // 内容高度
-    const navHeight = ref(0)
-    // 背景颜色
-    const containerStyle = ref('')
-    // 文字样式
-    const textStyle = ref('')
-    // 图标样式
-    const iconStyle = ref('')
+import {
+    onBeforeMount,
+    ref
+} from 'vue'
+const props = defineProps({
+    background: {
+        type: String,
+        default: 'rgba(255, 255, 255, 1)'
+    },
+    color: {
+        type: String,
+        default: 'rgba(0,0,0,1)'
+    },
+    fontSize: {
+        type: String,
+        default: '32'
+    },
+    iconWidth: {
+        type: String,
+        default: '116'
+    },
+    iconHeight: {
+        type: String,
+        default: '38'
+    }
+})
+onBeforeMount(() => {
+    console.log('onBeforeMount')
+    setNavSize()
+    setStyle()
+})
+// 状态栏高度
+const statusHeight = ref(0)
+// 内容高度
+const navHeight = ref(0)
+// 背景颜色
+const containerStyle = ref('')
+// 文字样式
+const textStyle = ref('')
+// 图标样式
+const iconStyle = ref('')
 
-    // 计算状态栏高度
-    const setNavSize = () => {
-        const {
-            system,
-            statusBarHeight
-        } = uni.getSystemInfoSync();
-        // console.log(systemInfo);
-        statusHeight.value = statusBarHeight;
-        const isios = system.indexOf('iOS') > -1;
-        if (isios) {
-            statusHeight.value = 96;
-        } else {
-            statusHeight.value = 88;
-        }
+// 计算状态栏高度
+const setNavSize = () => {
+    const {
+        system,
+        statusBarHeight
+    } = uni.getSystemInfoSync();
+    // console.log(statusBarHeight);
+    statusHeight.value = statusBarHeight;
+    const isios = system.indexOf('iOS') > -1;
+    if (isios) {
+        navHeight.value = 96;
+    } else {
+        navHeight.value = 48;
     }
-    // 样式设置
-    const setStyle = () => {
-        containerStyle.value = `background: ${props.background};`
-        textStyle.value = `color: ${props.color};font-size: ${props.fontSize}rpx;`
-        iconStyle.value = `width: ${props.iconWidth}rpx;height: ${props.iconHeight}rpx;`
-    }
+}
+// 样式设置
+const setStyle = () => {
+    containerStyle.value = `background: ${props.background};`
+    textStyle.value = `color: ${props.color};font-size: ${props.fontSize}rpx;`
+    iconStyle.value = `width: ${props.iconWidth}rpx;height: ${props.iconHeight}rpx;`
+}
 </script>
 
 <style>
-    .nav {
-        position: fixed;
-        width: 100%;
-        left: 0;
-        z-index: 2;
-        background: blueviolet;
-    }
+.nav {
+    position: fixed;
+    width: 100%;
+    top: 0;
+    left: 0;
+    z-index: 2;
+    background: #27c9de;
+}
 </style>
