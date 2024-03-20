22607522e8190b15c1dbb934d6a6360e257edeee
tayzhang <tayzhang@tencent.com>
2024-03-20 11:49:51 +0800

    finish info bar
    Signed-off-by: tayzhang <tayzhang@tencent.com>

--- medical-service-helper/pages/index/index.vue
+++ medical-service-helper/pages/index/index.vue
@@ -1,13 +1,27 @@
 <template>
-    <view class="content">
-        <navbar></navbar>
-        <button style="margin-top: 130rpx;"
-                @click="navigateTo">
-            跳转至搜索
-        </button>
+    <view>
+        <navbar :isHome="true"></navbar>
+        <view style="margin-top: 130rpx">
+            <view class="weui-cell"
+                  style="background:#fff9eb;">
+                <view class="weui-cell_hd">
+                    <image src="./../../static/resource/images/ic_myapp.png"
+                           mode="scaleToFill"
+                           style="display: block; width: 40rpx; height: 40rpx; margin-right: 14rpx;" />
+                </view>
+                <view class="weui-cell_bd">
+                    <text style="color:#be9719; font-size: 13px;">点击右上角"添加到我的小程序"方便下次找到!</text>
+                </view>
+                <view class="weui-cell_ft">
+                    <image src="./../../static/resource/images/modal_closer.png"
+                           style="display: block; width: 40rpx; height: 40rpx; margin-right: 14rpx;"
+                           mode="scaleToFill" />
+                </view>
+            </view>
+        </view>
     </view>
     <!-- <view>show index page</view> -->
-    <my-component></my-component>
+    <!-- <my-component></my-component> -->
 </template>
 
 <script setup>
@@ -19,10 +33,7 @@ const navigateTo = () => {
 </script>
 
 <style>
-.content {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+page {
+    background: white;
 }
 </style>
