9113640c9ee9299d2e397bc63ef2e654bb6c1adc
tayzhang <tayzhang@tencent.com>
2024-03-20 23:19:56 +0800

    swiper ads
    Signed-off-by: tayzhang <tayzhang@tencent.com>

--- medical-service-helper/pages/index/index.vue
+++ medical-service-helper/pages/index/index.vue
@@ -19,20 +19,70 @@
                 </view>
             </view>
         </view>
+        <view class="index-swiper"
+              v-if="slides && slides.length > 0">
+            <swiper :autoplay="true"
+                    :circular="true"
+                    :interval="3000"
+                    :duration="500"
+                    :indicator-dots="true">
+                <block v-for="(item, index) in slides"
+                       :key="index">
+                    <swiper-item>
+                        <image :src="item.pic_image_url"
+                               mode="widthFix"
+                               show-menu-by-longpress="true"
+                               :data-index='index' />
+                    </swiper-item>
+                </block>
+            </swiper>
+        </view>
     </view>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, reactive, computed } from 'vue';
 import { onLoad } from '@dcloudio/uni-app'
 const app = getApp()
+const slides = ref([])
 onLoad(() => {
     app.globalData.utils.getUserInfo()
+    app.globalData.utils.request({
+        url: '/app/init',
+        success: (res) => {
+            const { id } = res.data.area
+            if (id) {
+                app.globalData.utils.request({
+                    url: '/Index/index',
+                    data: { aid: id },
+                    success: (indexRsp) => {
+                        slides.value = indexRsp.data.slides
+                    }
+                })
+            }
+        }
+    })
 })
 </script>
 
 <style>
 page {
     background: white;
+}
+
+.index-swiper {
+    padding: 20rpx 20rpx 0 20rpx;
+    overflow: hidden;
+}
+
+.index-swiper swiper {
+    height: 320rpx;
+    overflow: hidden;
+    border-radius: 10rpx;
+}
+
+.index-swiper swiper image {
+    width: 100%;
+    height: 100%;
 }
 </style>
