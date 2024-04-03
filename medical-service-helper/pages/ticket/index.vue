d17f52dbcfa9e8a1bc84af10e480a9476b1d29b3
taylorzhang <taylorzyx@192.168.1.7>
2024-04-03 23:08:37 +0800

    Add new tab named ticket
    Signed-off-by: taylorzhang <taylorzyx@192.168.1.7>

---
+++ medical-service-helper/pages/ticket/index.vue
@@ -0,0 +1,38 @@
+<template>
+    <view>
+        <view>
+            <text>ticket list:</text>
+        </view>
+        <view v-if="tickets.length > 0">
+            <block v-for="ticket in tickets" :key="ticket.id">
+                <view>
+                    <text>{{ ticket.id }}</text>
+                    <text>{{ ticket.title }}</text>
+                </view>
+            </block>
+        </view>
+    </view>
+</template>
+
+<script setup>
+    import { ref, reactive, toRaw } from "vue";
+    import { onShow } from "@dcloudio/uni-app";
+    const app = getApp();
+    const tickets = ref([]);
+    // 页面周期函数--监听页面加载
+    onShow((options) => {
+        console.log("ticket load: This is a log message", options);
+        app.globalData.utils.request({
+            url: "/api/tickets",
+            header: {
+                Authorization: "Bearer " + uni.getStorageSync("AuthToken"),
+            },
+            success: (success) => {
+                console.log("Ticket request successful", success);
+                tickets.value = success;
+            },
+        });
+    });
+</script>
+
+<style></style>
